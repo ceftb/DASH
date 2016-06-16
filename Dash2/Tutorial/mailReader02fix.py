788a372539d02b99512df005409c69cd01416097
mmullin1 <mmullin17@windwardschool.org>
2016-06-16 10:23:24 -0700

    Update and rename mailReader02.py to mailReader02fix.py

--- Dash2/Tutorial/mailReader02fix.py
+++ Dash2/Tutorial/mailReader02fix.py
@@ -9,13 +9,14 @@ class MailReader(DASHAgent):
         self.readAgent("""
 goalWeight doWork 1
 goalRequirements doWork
+
   sendMail()
   readMail(newmail)
   processMail(newmail)
   sleep(1)
   forget([sendMail(),readMail(x),sleep(x)])  # a built-in that removes matching elements from memory
 transient doWork     # Agent will forget goal's achievement or failure as soon as it happens
-                       """)
+""")
         self.primitiveActions([('readMail', self.read_mail), ('sendMail', self.send_mail), ('processMail', self.process_mail)])
 
         # Using this as a counter in the email that gets sent
@@ -23,27 +24,20 @@ transient doWork     # Agent will forget goal's achievement or failure as soon a
 
     def read_mail(self, call):
         mail_var = call[1]
-        #[status, data] = self.sendAction("getMail")
+        [status, data] = ["", {'subject': 'buyTickets'}]
         print 'response to getMail is', status, data
-        if status == "success":
-            print "read mail success with", data
-            #return [{mail_var: data}]
-        else:
-            return []
+        print "read mail success with", data
+        return [{mail_var: data}]
 
     def send_mail(self, call):
         print 'send mail call', call
-        #[status, data] = self.sendAction("sendMail",
-                                        # [{'to': 'mailagent@amail.com', 'subject': 'test',
-                                          # 'body': 'this is test message ' + str(self.mailCounter)}])
+        data = ""
         self.mailCounter += 1
-        if status == "success":
-            print 'send mail success with data', data
-            return [{}]
-        else:
-            return []
+        print 'send mail success with data', data
+        return [{}]
 
     def process_mail(self, call):
+        print call
         mail = call[1]['subject']
         if mail == "buyTickets":
             print 'buys plane tickets', call
