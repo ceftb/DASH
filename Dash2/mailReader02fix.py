a587052cb649f03014671d10430fefd49e4875f9
mmullin1 <mmullin17@windwardschool.org>
2016-06-17 14:39:33 -0700

    Update mailReader02fix.py

--- Dash2/mailReader02fix.py
+++ Dash2/mailReader02fix.py
@@ -65,7 +65,7 @@ transient doWork     # Agent will forget goal's achievement or failure as soon a
     def process_mail(self, call):
         possible_destinations = ['New York', 'Paris', 'London', 'Shanghai']
         print [random.choice(possible_destinations), 'Friday']
-        return [random.choice(possible_destinations), 'Friday']
+        return [random.choice(possible_destinations), 'Friday']   #when running on terminal, it appears to only choose a destination once, and not repeat this action
 
     
     # def process_mail(self, call):
