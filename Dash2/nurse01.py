c297ed7f9d0285e828fc6f21b2b11d6ec1c28cf4
Marley M <melissamullin@guest-126.isi.edu>
2016-06-22 14:28:26 -0700

    updated

--- Dash2/nurse01.py
+++ Dash2/nurse01.py
@@ -1,6 +1,7 @@
 
 from dash import DASHAgent
 import time
+import random
 
 start_time = time.time()
 
@@ -25,25 +26,27 @@ goalRequirements doWork
         self.primitiveActions([('findRightMedication', self.find_right_medication),('logOn', self.log_on)])
 
     def log_on(self,call):
-        print 'logs on successfully' #try to have some variables like tiredness/frustration/busy/rightbed
+        print 'logs on successfully' #try to have some variables like tiredness/frustration/busy/rightbed, so could be unsuccessful or cant find computer or something
         return[{}]
-
+        #or doesn't?
 
 
     def find_right_medication(self,call):
-        while time <= 5:
-            print 'still logged in, right medication', call
-            return [{}]
-        if time > 5:
-            print 'login to a different patient account, wrong medication', call # or login again.
-            return [{}]
-        else:
-            print 'must log in again'
-            return []
+        if logon = 'success':
+            while time <= 5:
+                patient_numbers = ['patient1','patient2','patient3','patient4']
+                print 'still logged in, right medication for patient 1', call
+                return [{}]
+            if time > 5:
+                print 'login to a different patient account' +  'random.choice(patient_numbers)', call # or login again.
+                return [{}]
+            else:
+                print 'must log in again'
+                return []
 
 
-# maybe add some sort of variable to account for human error, even if logged in successfully and
+# maybe add some sort of variable to account for human error, even if logged in successfully 
 
 
-if _name_ == '_main_':
-    nurse().agentLoop()
+#if _name_ == '_main_':
+  #  nurse().agentLoop()
