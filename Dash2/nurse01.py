6e68ea13a67e8f250c09ab433081caaef83d30d8
Marley M <melissamullin@guest-126.isi.edu>
2016-06-23 10:54:35 -0700

    added functions

--- Dash2/nurse01.py
+++ Dash2/nurse01.py
@@ -2,6 +2,7 @@
 from dash import DASHAgent
 import time
 import random
+from collections import deque
 
 start_time = time.time()
 
@@ -37,27 +38,75 @@ goalRequirements findComputer(computer)
 
     """)
 
-        self.primitiveActions([('pickPatient', self.pick_patient), ('findRightMedication', self.find_right_medication),
-                               ('logOn', self.log_on)])
+        self.primitiveActions([('pickPatient', self.pick_patient), ('findMedications', self.find_medications),('deliverMedications',
+                               self.deliver_medications), ('logDelivery', self.log_delivery), ('findComputer', self.find_computer),
+                               ('readSpreadsheet', self.read_spreadsheet), ('writeSpreadsheet', self.call),
+                               ('alreadyLoggedOn', self.already_logged_on), ('logIn', self.log_in)])
 
-    def log_on(self,call):
-        print 'logs on successfully' #try to have some variables like tiredness/frustration/busy/rightbed, so could be unsuccessful or cant find computer or something
+    def pick_patient(self,call):
+        list = ('joe','harry','david','bob')
+        print ('successfully looks up medication for',list[0])
+        list.pop()
         return[{}]
-        #or doesn't?
 
 
-    def find_right_medication(self,call):
-        if logon = 'success':
-            while time <= 5:
-                patient_numbers = ['patient1','patient2','patient3','patient4']
-                print 'still logged in, right medication for patient 1', call
-                return [{}]
-            if time > 5:
-                print 'login to a different patient account' +  'random.choice(patient_numbers)', call # or login again.
-                return [{}]
-            else:
-                print 'must log in again'
-                return []
+    def find_medications(self, call):
+        medications = ['percocet','codeine','insulin','zithromycin']
+        #x = random.choice(medications) - attempt to find someway to reference this later
+        print ('find list of medications' + random.choice(medications))
+        return [{call[1]: random.choice(medications)}]
+
+    def deliver_medications(self, call):
+        print 'delivers medication to patient'
+        return[]
+
+    def log_delivery(self, call):
+        print "logs delivery of medications into computer"
+        return[]
+
+    def find_computer(self, call):
+        computer = call[1]
+        if computer is 'available':
+            print ' logs into computer successfully'
+        elif computer is 'unavailable':
+            print 'logs in, but logs another nurse out'
+        return []
+
+    def  write_spreadsheet(self, call):
+        patient = pick_patient
+        medications = deliver_medications  # somehow, actually record patient and med info
+        print ('opens spreadsheet and write patient info:' + (medications) + (patient))  # in hub version, somehow have to actually record it
+        return []
+
+    def already_logged_on(self, call):
+        if time < 5:
+            print 'already logged into computer'
+            return[{}]
+
+    def log_in(self, call):
+        if time > 5:
+            print'login to new computer'
+            return[{}]
+
+
+    # def log_on(self,call):
+    #     print 'logs on successfully' #try to have some variables like tiredness/frustration/busy/rightbed, so could be unsuccessful or cant find computer or something
+    #     return[{}]
+    #     #or doesn't?
+    #
+    #
+    # def find_right_medication(self,call):
+    #     if logon = 'success':
+    #         while time <= 5:
+    #             patient_numbers = ['patient1','patient2','patient3','patient4']
+    #             print 'still logged in, right medication for patient 1', call
+    #             return [{}]
+    #         if time > 5:
+    #             print 'login to a different patient account' +  'random.choice(patient_numbers)', call # or login again.
+    #             return [{}]
+    #         else:
+    #             print 'must log in again'
+    #             return []
 
 # maybe add some sort of variable to account for human error, even if logged in successfully 
 
