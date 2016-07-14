db4ba379565aaa691abcf14c9d6ce4e14e5d3ad6
jimblythe <blythejim@gmail.com>
2016-07-13 17:12:49 -0700

    Default processSendActionRequest on the hub looks for a method whose name matches the action, either directly or by adjusting camel case to underscores, and then executes the method. This allows subclasses of hubs to inherit their behavior without specific coding.

--- Dash2/nurse_hub.py
+++ Dash2/nurse_hub.py
@@ -12,22 +12,11 @@ class NurseHub(WorldHub):
         self.logged_out = [None for i in range(0, self.number_of_computers)]
         self.possible_medications = ['_percocet', '_codeine', '_insulin', '_zithromycin']
 
-    def processSendActionRequest(self, agent_id, action, data):
-        print "nurse hub processing action", action, data
-        if action == "findOpenComputers":    # return a list of computers that noone is logged into
-            return 'success', [i for i in range(1, self.number_of_computers+1) if self.logged_on[i-1] is None]
-        elif action == "findAllComputers":   # return a list of all computers, whether another agent is logged in or not
-            return 'success', range(1, self.number_of_computers + 1)
-        elif action == "login":
-            return self.login(agent_id, data)
-        elif action == "logout":
-            return self.logout(agent_id, data)
-        elif action == "readSpreadsheet":
-            return self.read_spreadsheet(agent_id, data)
-        elif action == "writeSpreadsheet":
-            return self.write_spreadsheet(agent_id, data)
-        else:
-            print "Unknown action:", action
+    def find_open_computers(self, agent_id, data):
+        return 'success', [i for i in range(1, self.number_of_computers+1) if self.logged_on[i-1] is None]
+
+    def find_all_computers(self, agent_id, data):
+        return 'success', range(1, self.number_of_computers + 1)
 
     def login(self, agent_id, data):
         print "Logging in", agent_id, "with", data
@@ -47,9 +36,6 @@ class NurseHub(WorldHub):
         # Don't really do anything for now. A log of the hub actions would record this write action for analysis
         return 'success'
 
-
-        # Marley to implement
- # something like ['success', [i for i in range(1, self.number_of_computers+1) if self.logged_on[i-1] == None]
 
 if __name__ == "__main__":
     NurseHub().run()
