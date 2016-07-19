93e1a8eb412dd4f547c4fe7e563a97af69a6664e
jimblythe <blythejim@gmail.com>
2016-07-19 08:43:27 -0700

    Nurse experiment file dovetails n nurses by running one step at a time from agentLoop, which now returns the chosen action to show whether the agent has finished

--- Dash2/nurse01.py
+++ Dash2/nurse01.py
@@ -5,7 +5,7 @@ import random
 
 class Nurse(DASHAgent):
 
-    def __init__(self):
+    def __init__(self, patients=['_joe', '_harry', '_david', '_bob']):
         DASHAgent.__init__(self)
 
         self.readAgent("""
@@ -21,12 +21,14 @@ goalRequirements doWork
 
 goalRequirements findMedications(patient, medications)
     findComputer(computer)
+    loadSpreadsheet(patient, computer)
     readSpreadsheet(patient, computer, medications)
 
 goalRequirements logDelivery(patient, medications)
     findComputer(computer)
+    loadSpreadsheet(patient, computer)
     writeSpreadsheet(patient, computer, medications)
-    logOut(computer)
+    # logOut(computer)   # Skip logging out to see what happens
 
 goalRequirements findComputer(computer)
     alreadyLoggedOn(computer)
@@ -39,91 +41,38 @@ transient doWork
     """)
 
         self.primitiveActions([('pickPatient', self.pick_patient), ('deliverMedications', self.deliver_medications),
+                               ('loadSpreadsheet', self.load_spreadsheet),
                                ('readSpreadsheet', self.read_spreadsheet), ('writeSpreadsheet', self.write_spreadsheet),
                                ('alreadyLoggedOn', self.already_logged_on), ('logIn', self.log_in),
                                ('logOut', self.log_out)])
         # self.traceAction = True  # uncomment to see more about the internal actions chosen by the agent
         self.register()
 
-        self.patient_list = ['_joe', '_harry', '_david', '_bob']
+        self.patient_list = patients
         self.computer = None    # computer the agent believes it's logged into
-
-
-#    def output(selffilename, sheet, list1, list2, x, y):
-#        book = xlwt.Workbook() # attempt tp put in the excel sheet
-#        sh = book.add_sheet(sheet)
-#        #sheet1 = book.add_sheet("Sheet1")
-#
-#        variables = [x, y]
-#        x_desc = 'patient'
-#        y_desc = 'medication'
-#        desc = [x_desc, y_desc]
-#
-#        col1_name = 'patient'
-#        col2_name = 'medication'
-#
-#        for n, v_desc, v in enumerate(zip(desc,variables)):
-#            sh.write(n,0, v_desc)
-#            sh.write(n, 1, v)
-#
-#        n+=1
-#
-#        sh.write(n,0, col1_name)
-#        sh.write(n, 1, col2_name)
-#
-#        for m, e1 in enumerate(list1, n+1):
-#            sh.write(m,0,e1)
-#
-#        for m, e2 in enumerate(list2, n+1):
-#            sh.write(m, 1, e2)
-#
-#        book.save(filename)
-#
-#        #^ from overflow. Other source: https://automatetheboringstuff.com/chapter12/
-#
-#        # sheet1.write(1, 'patient', 'medication')
-#        # sheet1.write(2, 'patient','medication')
-#        # sheet1.write(3, 'patient','medication')
-#
-#        # i=4
-#        #
-#        # for n in list1:
-#        #     i = i+1
-#        #     sheet1.write(i, 0, n)
 
     def pick_patient(self, (goal, patient_variable)):
         if self.patient_list:
-            patient = self.patient_list.pop() # probably need to import something, can't figure out what
-            print 'successfully looks up medication for', patient
+            patient = self.patient_list.pop()  # probably need to import something, can't figure out what
+            print 'starting to work on', patient
             return [{patient_variable: patient}]
         else:
             print "No more patients!"
             return []
 
-#    def find_medications(self, call):
-#        medications = ['percocet', 'codeine', 'insulin', 'zithromycin']
-#        #x = random.choice(medications) - attempt to find someway to reference this later
-#        print ['find list of medications', random.choice(medications)], call
-#        return [{call[2]: random.choice(medications)}]
-
     def deliver_medications(self, (goal, patient, medication)):
         print 'delivers', medication, 'to patient', patient
         return [{}]
 
-    # def find_computer(self, call):
-    #     # computer = call[1]
-    #     # if computer is 'available': #is there a way to assign a probability to this?
-    #     #     print ' logs into computer successfully'
-    #     # elif computer is 'unavailable':
-    #     #     print 'logs in, but logs another nurse out'
-    #     print 'logs in successfully into a computer' #in this case their is only 1 computer
-    #     return [{call[1]: 1}]    # Just using '1' to denote the only computer we need to worry about in this version
+    def load_spreadsheet(self, (predicate, patient, computer)):
+        self.sendAction("loadSpreadsheet", [patient, computer])
+        return [{}]
 
     def read_spreadsheet(self, (predicate, patient, computer, medications_variable)):
         print 'reading the patient spreadsheet for', patient, 'on', computer
-        result = self.sendAction("readSpreadsheet", [patient, computer])   # returns the medication for the patient
-        if result is not None and result[0] == 'success':
-            return [{medications_variable: result[1]}]
+        [status, medication] = self.sendAction("readSpreadsheet", [patient, computer])   # returns the medication for the patient
+        if status == 'success':
+            return [{medications_variable: medication}]
         else:
             return []
 
@@ -164,25 +113,6 @@ transient doWork
         self.sendAction("login", [self.computer])
         return[{computer_variable: self.computer}]
 
-
-    # def log_on(self,call):
-    #     print 'logs on successfully' #try to have some variables like tiredness/frustration/busy/rightbed, so could be unsuccessful or cant find computer or something
-    #     return[{}]
-    #     #or doesn't?
-    #
-    #
-    # def find_right_medication(self,call):
-    #     if logon = 'success':
-    #         while time <= 5:
-    #             patient_numbers = ['patient1','patient2','patient3','patient4']
-    #             print 'still logged in, right medication for patient 1', call
-    #             return [{}]
-    #         if time > 5:
-    #             print 'login to a different patient account' +  'random.choice(patient_numbers)', call # or login again.
-    #             return [{}]
-    #         else:
-    #             print 'must log in again'
-    #             return []
 
 # maybe add some sort of variable to account for human error, even if logged in successfully 
 
