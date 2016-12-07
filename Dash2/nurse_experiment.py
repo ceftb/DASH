de0f9496c39d304073ec2448b0507f83c88650c1
jimblythe <blythejim@gmail.com>
2016-12-07 11:34:55 -0800

    Small change to printed output

--- Dash2/nurse_experiment.py
+++ Dash2/nurse_experiment.py
@@ -6,6 +6,7 @@ from nurse_hub import Event
 
 # To see the results, start a nurse_hub and run this file.
 
+
 class NurseTrial(Trial):
 
     def __init__(self, num_nurses=20, num_patients=5, num_computers=10, num_medications=10):
@@ -27,14 +28,15 @@ class NurseTrial(Trial):
         self.experiment_client.sendAction("initWorld", (self.num_computers, self.num_medications))
 
         # Set up the agents. Each of n nurses is given a different list of k patients
-        self.agents = [nurse01.Nurse(ident=n, patients=["_p_" + str(n) + "_" + str(p) for p in range(1, self.num_patients + 1)])
+        self.agents = [nurse01.Nurse(ident=n,
+                                     patients=["_p_" + str(n) + "_" + str(p)for p in range(1, self.num_patients + 1)])
                        for n in range(0, self.num_nurses)]
         for agent in self.agents:
             agent.active = True
 
     def process_after_agent_action(self, agent, action):
         if action is None:
-            print 'agent', agent, 'action is None, stopping'
+            print 'agent', agent, 'found no action, stopping'
             agent.active = False
 
     def agent_should_stop(self, agent):
@@ -60,7 +62,7 @@ exp.run()
 trial = exp.trial_outputs[0]
 for event in trial.events:
     print "!!" if event.patient != event.spreadsheet_loaded else "", event
-print trial.misses, "misses out of", len(trial.events)
+print trial.misses, "misses out of", len([e for e in trial.events if e.type in ["write", "read"]]), "reads and writes"
 # Find out which computers were used most heavily
 computer_events = {}
 computer_misses = {}
