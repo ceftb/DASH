8fa34a46c6418760a618f21a2ab818be657bda11
atregubov <tregubov@usc.edu>
2018-01-11 16:16:32 -0800

    Bugxing in LeadDevelperAgent and in GitUserAgent. Activity generate_random_repo_name removed from goal requrements in GitUserAgent. GitRepoHub now handles pull requests.

--- Dash2/github/github_lead_developer_agent.py
+++ Dash2/github/github_lead_developer_agent.py
@@ -15,66 +15,21 @@ class GitLeadDeveloperAgent(GitUserAgent):
         # Goals
         self.readAgent(
             """
-goalWeight ReviewCode 1
+goalWeight ReviewCode 2
 
 goalRequirements ReviewCode
-  pull_repo_event(_myRepoName)
-  commit_comment_event(_myRepoName, 'code review commit')
-            """)
-
-        # Registration
-        self.server_host = kwargs.get("host", "localhost")
-        self.server_port = kwargs.get("port", 5678)
-        registration = self.register()
-
-        # Setup information
-        self.type = kwargs.get("type", "user")
-        self.login_h = kwargs.get("login_h", None)
-        self.ght_id_h = kwargs.get("ght_id_h", None)
-        self.company = kwargs.get("company", "")
-        self.location = kwargs.get("location", "")
-        self.created_at = kwargs.get("created_at", None)
-        self.created_dow = kwargs.get("created_dow", None)
-        self.fake = kwargs.get("fake", False)
-        self.deleted = kwargs.get("deleted", False)
-        self.lat = kwargs.get("lat", None)
-        self.lon = kwargs.get("lon", None)
-        self.state = kwargs.get("state", None)
-        self.city = kwargs.get("city", None)
-        self.country_code = kwargs.get("country_code", None)
-        self.site_admin = kwargs.get("site_admin", False)
-        self.public_repos = kwargs.get("public_repos", 0)
-        self.followers = kwargs.get("followers", 0)
-        self.following = kwargs.get("following", 0)
-
-        # Assigned information
-        self.id = registration[1]
-        # I want an internal Global time here, but not clear how to assign it
-
-        # Other Non-Schema information
-        self.following_list = {}  # ght_id_h: {full_name_h, following_date, following_dow}
-        self.watching_list = {}  # ght_id_h: {full_name_h, watching_date, watching_dow}
-        self.owned_repos = set()  # ght_id_h
-        self.name_to_repo_id = dict()  # hub expects ids in calls other than create_repo_event, not names
+  create_repo_event(RepoName)
+  commit_comment_event(RepoName, 'code review commit')
+  pick_random_repo(RepoName)
+  pull_repo_event(RepoName)
+              """)
 
         # Actions
         self.primitiveActions([
-            ('create_repo_event', self.create_repo_event),
-            ('commit_comment_event', self.commit_comment_event),
-            ('create_tag_event', self.create_tag_event),
-            ('create_branch_event', self.create_branch_event),
-            ('delete_tag_event', self.delete_tag_event),
-            ('delete_branch_event', self.delete_branch_event),
-            ('fork_event', self.fork_event),
-            ('issue_comment_event', self.issue_comment_event),
-            ('pull_request_event', self.pull_request_event),
-            ('push_event', self.push_event),
-            ('watch_event', self.watch_event),
-            ('follow_event', self.follow_event),
-            ('member_event', self.member_event),
+            ('pull_repo_event', self.pull_repo_event),
             ('public_event', self.public_event)])
 
-
+        self.traceGoals = True
 
     ############################################################################
     # Lead developer git user methods
@@ -90,12 +45,19 @@ goalRequirements ReviewCode
             return []
         status = self.sendAction("commit_comment_event", (self.name_to_repo_id[repo_name], comment))
         print 'commit comment event:', status, repo_name, self.name_to_repo_id[repo_name], comment
+        return [{}]
 
     def pull_repo_event(self, args):
         """
         This event describes local code update (pull from the repo)
         """
-        pass
+        _, repo_name = args
+        if repo_name not in self.name_to_repo_id:
+            print 'Agent does not know the id of the repo with name', repo_name, 'cannot commit'
+            return []
+        status = self.sendAction("pull_repo_event", (self.name_to_repo_id[repo_name]))
+        return [{}]
+
 
 if __name__ == '__main__':
     """
