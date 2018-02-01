cb12e0265797a30663e1c16c7ab1348b3088140b
atregubov <tregubov@usc.edu>
2018-02-01 15:48:48 -0800

    Zookeeper installation script updated.

--- Dash2/distributed_github/zookeeper_install.sh
+++ Dash2/distributed_github/zookeeper_install.sh
@@ -2,19 +2,14 @@
 # This scrip installs Apache Zookeeper and Kazoo python library (zookeeper client API for Python). 
 # Tested on Ubuntu 16.04 LTS
 
-# $REPO_CLONES_HOME is a local path where cloned repositories and installation archives are stored.
+# $KAZOO_CLONE is a local path where kazoo is cloned (git clone https://github.com/2600hz/kazoo.git  ).
 sudo apt-get install mc --yes
  
-REPO_CLONES_HOME=~/projects 
+KAZOO_CLONE=~/projects/kazoo 
 ZK_CONF=/etc/zookeeper/conf/zoo.cfg 
 ZK_ID=/etc/zookeeper/conf/myid
-USR=$USER
 
-# The following proejcts (reposiztory clones) are requred to be in $REPO_CLONES_HOME
-# - kazoo
-# - webdash
-
-cd $REPO_CLONES_HOME/kazoo
+cd $KAZOO_CLONE
 # install kazoo 
 echo "installing kazoo ..."
 sudo apt-get install python-setuptools --yes
@@ -34,6 +29,7 @@ sudo apt-get install zookeeperd --yes
 sudo echo 1 > $ZK_ID
 sudo cat ./zoo.conf > $ZK_CONF
 
+echo "restarting zookeeper server ..."
 sudo service zookeeper stop
 sudo service zookeeper start
 
