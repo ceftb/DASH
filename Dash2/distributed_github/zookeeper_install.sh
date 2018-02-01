bc915550c9a0544194985834674f8ab37176518d
atregubov <tregubov@usc.edu>
2018-02-01 14:15:37 -0800

    Zookeeper installation files updated.

--- Dash2/distributed_github/zookeeper_install.sh
+++ Dash2/distributed_github/zookeeper_install.sh
@@ -3,11 +3,12 @@
 # Tested on Ubuntu 16.04 LTS
 
 # $REPO_CLONES_HOME is a local path where cloned repositories and installation archives are stored.
-
+sudo apt-get install mc --yes
  
 REPO_CLONES_HOME=~/projects 
 ZK_CONF=/etc/zookeeper/conf/zoo.cfg 
 ZK_ID=/etc/zookeeper/conf/myid
+USR=$USER
 
 # The following proejcts (reposiztory clones) are requred to be in $REPO_CLONES_HOME
 # - kazoo
@@ -16,27 +17,27 @@ ZK_ID=/etc/zookeeper/conf/myid
 cd $REPO_CLONES_HOME/kazoo
 # install kazoo 
 echo "installing kazoo ..."
-sudo apt-get install python-setuptools
+sudo apt-get install python-setuptools --yes
 sudo python setup.py install
 
 # install zookeeper 
 echo "installing zookeeper base ..."
-sudo apt-get install zookeeper
+sudo apt-get install zookeeper --yes
 
 # TBD: replace this part with standalone package of zookeeper
 # install zookeeper server
 echo "installing zookeeper server ..."
-sudo apt-get install zookeeperd
+sudo apt-get install zookeeperd --yes
 
 # set up conf file and node id
 # need root access to write in /etc
 sudo su 
 echo 1 > $ZK_ID
 cat ./zoo.conf > $ZK_CONF
-exit
+sudo $USR
 
-sudo 
-# node1.kazoo-experiment.DASH.isi.deterlab.net
+sudo service zookeeper stop
+sudo service zookeeper start
 
 
 
