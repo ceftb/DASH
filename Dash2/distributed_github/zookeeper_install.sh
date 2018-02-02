910e71880b4a1ea2b16def7496a2b3465ebc3d63
atregubov <tregubov@usc.edu>
2018-02-02 12:12:52 -0800

    Zookeeper installation script updated.

--- Dash2/distributed_github/zookeeper_install.sh
+++ Dash2/distributed_github/zookeeper_install.sh
@@ -2,11 +2,15 @@
 # This scrip installs Apache Zookeeper and Kazoo python library (zookeeper client API for Python). 
 # Tested on Ubuntu 16.04 LTS
 
-# $KAZOO_CLONE is a local path where kazoo is cloned (git clone https://github.com/2600hz/kazoo.git  ).
-sudo apt-get install mc --yes
- 
+# NUMBER_OF_NODES - number of nodes in cluster
+NUMBER_OF_NODES=7
+
+# KAZOO_CLONE is a local path where kazoo is cloned (git clone https://github.com/2600hz/kazoo.git  ).
 KAZOO_CLONE=~/projects/kazoo 
+
+# WEBDASH_CLONE is a local path where webdash is cloned (git clone https://github.com/cuts/webdash.git  ).
 WEBDASH_CLONE=~/projects/webdash 
+
 ZK_CONF=/etc/zookeeper/conf/zoo.cfg 
 ZK_ID=/etc/zookeeper/conf/myid
 
@@ -32,6 +36,14 @@ sudo chmod go+rw $ZK_ID
 sudo uname -n | sed 's/[^0-9]*//g' > $ZK_ID
 sudo chmod go+rw $ZK_CONF
 sudo cat $WEBDASH_CLONE/Dash2/distributed_github/zoo.conf > $ZK_CONF
+# appending other nodes
+
+ID=1
+while [  $ID -le $NUMBER_OF_NODES ]; do
+	echo "id is " $ID
+	echo server.$ID=node$ID:2888:3888 >> $ZK_CONF
+	let ID=ID+1 
+done
 
 echo "restarting zookeeper server ..."
 sudo service zookeeper stop
@@ -39,4 +51,6 @@ sudo service zookeeper start
 
 echo "Zookeeper installation completed"
 
+# echo "installing mc .."
+# sudo apt-get install mc --yes
 
