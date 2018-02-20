654d5faeec75d3d764089466ef323b9e0bbd60ab
alexey <tregubov@usc.edu>
2018-02-20 11:14:12 -0800

    minor bugfix in zk_git_hub_experiment.py (converted number of nodes to int)

--- Dash2/distributed_github/zk_github_experiment.py
+++ Dash2/distributed_github/zk_github_experiment.py
@@ -84,10 +84,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         pass
     elif len(sys.argv) == 1:
-        number_of_hosts = sys.argv[1]
+        number_of_hosts = int(sys.argv[1])
     elif len(sys.argv) == 3:
         zk_hosts = sys.argv[1]
-        number_of_hosts = sys.argv[2]
+        number_of_hosts = int(sys.argv[2])
     else:
         print 'incorrect arguments: ', sys.argv
 
