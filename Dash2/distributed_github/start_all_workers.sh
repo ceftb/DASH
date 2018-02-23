34e85333c63603993e853f818d5c6d0b88109dc7
alexey <tregubov@usc.edu>
2018-02-23 15:24:38 -0800

    Fixed typo

--- Dash2/distributed_github/start_all_workers.sh
+++ Dash2/distributed_github/start_all_workers.sh
@@ -19,7 +19,7 @@ function start_all {
 	for ID in `seq 1 $NUMBER_OF_NODES`;
 	do
 		echo 'Starting worker on node ' ${NODES[$ID-1]}
-		ssh ${NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/distributed_github/start_workers.sh $ID $WEBDASH_CLONE"
+		ssh ${NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/distributed_github/start_all_workers.sh $ID $WEBDASH_CLONE"
 	done
 	echo 'workers are running'
 }
