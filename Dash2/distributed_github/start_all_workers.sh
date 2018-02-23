088844717cfc0f487806eed993c405e2a4b32f75
alexey <tregubov@usc.edu>
2018-02-23 11:02:49 -0800

    Installation and deployment scripts were updated.

--- Dash2/distributed_github/start_all_workers.sh
+++ Dash2/distributed_github/start_all_workers.sh
@@ -7,7 +7,7 @@ echo 'Total ' $NUMBER_OF_NODES ' workers in assemble'
 for ID in `seq 1 $NUMBER_OF_NODES`;
         do
                 echo 'Starting worker on node ' ${NODES[$ID-1]}
-		ssh ${NODES[$ID-1]} "tmux new-session -d $WEBDASH_CLONE/Dash2/distributed_github/start_single_worker.sh $ID"
+		ssh ${NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/distributed_github/start_single_worker.sh $ID"
         done
 
 echo 'workers are running'
--- Dash2/distributed_github/zookeeper_install_all.sh
+++ Dash2/distributed_github/zookeeper_install_all.sh
@@ -9,7 +9,7 @@ echo 'Total ' $NUMBER_OF_NODES ' workers in assemble'
 for ID in `seq 1 $NUMBER_OF_NODES`;
         do
                 echo 'Installing Zookeeper on node ' ${NODES[$ID-1]}
-                ssh ${NODES[$ID-1]} "tmux new-session -d $WEBDASH_CLONE/Dash2/distributed_github/zookeeper_install.sh $ID"
+                ssh ${NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/distributed_github/zookeeper_install.sh $ID"
         done
 
 echo "Zookeeper installation completed on all nodes"
