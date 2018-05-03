5f5d5c1d629fa863fe99defa50d5ee9d1bf7c517
Alexey Tregubov <tregubov@n1.mca.dash.isi.deterlab.net>
2018-05-03 10:49:16 -0700

    kazoo_install.sh was updated. ijson library was added to the installation script.

--- Dash2/github/kazoo_install.sh
+++ Dash2/github/kazoo_install.sh
@@ -10,7 +10,7 @@ function install_on_all_nodes {
 	for ID in `seq 1 $NUMBER_OF_NODES`;
 		do
 			echo 'Installing Zookeeper on node ' ${DASH_NODES[$ID-1]}
-			ssh ${DASH_NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/github/kazoo_install.sh $ID $KAZOO_CLONE $TMP_DIR"
+			ssh ${DASH_NODES[$ID-1]} "tmux new-session -d bash $WEBDASH_CLONE/Dash2/github/kazoo_install.sh $ID $KAZOO_CLONE $TMP_DIR $IJSON_CLONE"
 		done
 	
 	echo "Kazoo installation completed on all DASH nodes"
@@ -20,6 +20,7 @@ function install_single_kazoo_instance {
 	CURR_NODE_ID=$1
 	KAZOO_CLONE=$2
 	TMP_DIR=$3
+	IJSON_CLONE=$4
 
 	cp -R $KAZOO_CLONE $TMP_DIR/kazoo_clone_$CURR_NODE_ID
 	cd $TMP_DIR/kazoo_clone_$CURR_NODE_ID
@@ -35,13 +36,16 @@ function install_single_kazoo_instance {
 	sudo apt-get install python-pip --yes
 	echo "installing python-numpy python-scipy ..."
 	sudo apt-get install python-numpy python-scipy --yes
-
 	pip freeze | grep kazoo >> $TMP_DIR/kazoo_report_$CURR_NODE_ID.txt
+	
+	cd $IJSON_CLONE 
+	echo $IJSON_CLONE2 >> $TMP_DIR/ijson_report_$CURR_NODE_ID.txt 1>> $TMP_DIR/ijson_report_$CURR_NODE_ID.txt
+	sudo python setup.py install  2>> $TMP_DIR/ijson_report_$CURR_NODE_ID.txt 1>> $TMP_DIR/ijson_report_$CURR_NODE_ID.txt
 }
 
 if [ -z "$1" ]; then
 	install_on_all_nodes
 else
-	install_single_kazoo_instance $1 $2 $3
+	install_single_kazoo_instance $1 $2 $3 $4
 fi
 
