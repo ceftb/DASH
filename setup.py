a1c0d0872b41c4b06e4cf141862bac2a2f5ca991
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2018-05-16 18:06:24 -0400

    added numpy dependency to setup.py since it is used in a couple places

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='Dash2',
       author='Jim Blythe <blythe@isi.edu>', # please change as you see fit
       license='MIT',                        # please change as you see fit. 
       packages=find_packages(exclude=['papers', 'dash-java', 'docs', 'tests']),
+      install_requires=['numpy']
 )
