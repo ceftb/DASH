23b40c716aafbae442960761f89d54391e117743
alexey <tregubov@usc.edu>
2018-07-02 13:29:35 -0700

    minor bugfix

--- Dash2/github/git_isi_agent.py
+++ Dash2/github/git_isi_agent.py
@@ -108,9 +108,11 @@ class ISIMixin(GitUserMixin):
         popularity = 0
         for own_repo in self.decision_data.owned_repos:
             popularity += self.hub.graph.nodes[own_repo]["pop"]
-        popularity = popularity / len(self.decision_data.owned_repos)
         if popularity == 0:
             popularity = 1
+        else:
+            popularity = popularity / len(self.decision_data.owned_repos)
+
         self.hub.graph.add_node(new_repo_id, shared=0, isU=0, pop=popularity) # TBD non zero popularity
         self.hub.graph.add_edge(new_repo_id, self.decision_data.id, own=1, weight= 1)
 
