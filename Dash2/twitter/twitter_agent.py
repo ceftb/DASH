a492c954dfe5c2ae333f0e5610c50a9f33c91ddf
alexey <tregubov@usc.edu>
2018-10-25 02:27:22 -0700

    Twitter agent - parent id and root id were added (for dryrun)

--- Dash2/twitter/twitter_agent.py
+++ Dash2/twitter/twitter_agent.py
@@ -1,6 +1,8 @@
+import random
 from Dash2.core.dash import DASHAgent
 from Dash2.socsim.socsim_agent import SocsimDecisionData, SocsimMixin
-from Dash2.socsim.output_event_log_utils import event_types, sort_data_and_prob_to_cumulative_array, random_pick_sorted
+from Dash2.socsim.event_types import twitter_events, twitter_events_list
+from Dash2.socsim.output_event_log_utils import sort_data_and_prob_to_cumulative_array, random_pick_sorted
 
 class TweetActionPair:
     def __init__(self, event_index, repo_id):
@@ -10,28 +12,8 @@ class TweetActionPair:
 class TwitterDecisionData(SocsimDecisionData):
 
     def initialize_using_user_profile(self, profile, hub):
+        SocsimDecisionData.initialize_using_user_profile(self, profile, hub)
 
-        # initialize:
-        # id, event_rate, last_event_time, own_repos, not_own_repos, all_known_repos,
-        # probabilities of event-repo pairs, popularity
-        self.id = profile.pop("id", None)
-        self.event_rate = profile.pop("r", 5)  # number of events per month
-
-        # frequency of use of associated repos:
-        tweet_id_to_freq = {int(repo_id) : int(freq["f"]) for repo_id, freq in profile["all_repos"].iteritems()}
-        self.all_known_tweets = []
-        self.all_known_tweets.extend(tweet_id_to_freq.iterkeys())
-
-        self.last_event_time = profile["let"]
-
-        self.popularity = profile.pop("pop", 0)
-
-        self.tweet_action_pairs = [TweetActionPair(ep[0], ep[1]) for ep in profile["erp"]]
-        self.tweet_action_probabilities = profile["erf"]
-        sum_ = sum(self.tweet_action_probabilities)
-        self.tweet_action_probabilities = [float(v) / float(sum_) for v in self.tweet_action_probabilities]
-        # sort:
-        self.tweet_action_pairs, self.tweet_action_probabilities = sort_data_and_prob_to_cumulative_array(self.tweet_action_pairs, self.tweet_action_probabilities)
 
 class TwitterMixin(SocsimMixin):
 
@@ -42,13 +24,25 @@ class TwitterMixin(SocsimMixin):
         SocsimMixin.__init__(self, **kwargs)
 
     def customAgentLoop(self):
-        # If control passes to here, the decision on choosing a user has already been made.
-        pair = random_pick_sorted(self.decision_data.tweet_action_pairs, self.decision_data.tweet_action_probabilities)
-        selected_event = event_types[pair.event_index]
-        selected_tweet = pair.repo_id
+        pair = random_pick_sorted(self.decision_data.event_res_pairs, self.decision_data.event_res_pairs_prob)
+        selected_event = twitter_events_list[pair.event_index]
+        selected_res = pair.res_id
 
-        self.hub.log_event(self.decision_data.id, selected_tweet, selected_event, None, self.hub.time)
-        self.decision_data.total_activity += 1
+        # additional_attributes
+        if selected_event == "tweet":
+            parent_id = selected_res
+            root_id = selected_res
+        else:
+            parent_id = 2000000 + random.randint(1, 1000)  # TBD need to fix
+            root_id = parent_id
+
+        parent_id = self.hub._try_to_convert_resource_id_to_original_id(parent_id)
+        root_id = self.hub._try_to_convert_resource_id_to_original_id(root_id)
+
+        additional_attributes = {"parentID": parent_id, "rootID": root_id}
+
+        self.hub.log_event(self.decision_data.id, selected_res, selected_event, self.hub.time,
+                           additional_attributes=additional_attributes)
 
         return False
 
