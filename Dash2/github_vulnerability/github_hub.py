5586aba626e2b28f4948d7fbb8507a08a1365e18
alexey <tregubov@usc.edu>
2018-11-01 10:17:55 -0700

    Added package for github vulnerability experiments.

---
+++ Dash2/github_vulnerability/github_hub.py
@@ -0,0 +1,21 @@
+import sys; sys.path.extend(['../../'])
+from Dash2.socsim.socsim_hub import SocsimHub
+from Dash2.socsim.output_event_log_utils import random_pick_sorted
+import datetime
+
+
+# Zookeeper hub for github agents
+class GithubHub(SocsimHub):
+    """
+    A class that handles client requests and modifies the desired repositories
+    """
+
+    sync_event_counter = 0
+
+    def __init__(self, zk, task_full_id, start_time, output_file_name):
+        SocsimHub.__init__(self, zk, task_full_id, start_time, output_file_name)
+
+    # this overrides default log method in SocsimHub
+    def log_event(self, user_id, resource_id, event_type, time, additional_attributes=None):
+        self.print_to_csv_log(user_id, resource_id, event_type, self._convert_time(time), additional_attributes)
+        self.print_to_json_log(user_id, resource_id, event_type, int(time), additional_attributes)
